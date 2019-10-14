6daffae412530f44e06f7d62f2f2a04a22d81ab8
Emily Chaplais <emilychaplais@hotmail.com>
2019-10-14 12:21:28 +0100

    created index.js files for testData, devData and all data

---
+++ db/data/development-data/index.js
@@ -0,0 +1,6 @@
+const articleData = require('./articles');
+const commentsData = require('./comments');
+const topicsData = require('./topics');
+const userData = require('./users');
+
+module.exports = { articleData, commentsData, topicsData, userData };
